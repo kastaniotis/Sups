33a580d74fb38e51c643cdf73bbd025427a5670c
Dimitris Kastaniotis <d.kastaniotis@iconic.gr>
2023-11-30 02:17:47 +0200

    multi arch publish

--- Sups/publish.arm64.sh
+++ Sups/publish.arm64.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-dotnet publish -r linux-arm64 -o ./public/arm64 -p:DebugType=none
-cd public/x64
+dotnet publish -r linux-arm64 -o ./public/arm64 -p:DebugType=none -c Release -p:PublishTrimmed=true -p:EnableCompressionSingleFile=true --self-contained -p:PublishAot=true
+cd public/arm64
 tar -czvf sups.tar.gz ./sups
