d82fe4465cce4515efa28bd61cf705b070b127c4
Dimitris Kastaniotis <d.kastaniotis@iconic.gr>
2023-12-01 17:57:36 +0200

    building housekeeping

--- Sups/install.sh
+++ Sups/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./publish.sh
-sudo cp ./public/sups /usr/local/bin
+./publish.all.sh
+sudo cp ./public/x64/sups /usr/local/bin
---
+++ Sups/publish.all.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+./publish.x64.sh
+./publish.arm64.sh
+./publish.arm32.sh
